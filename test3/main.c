a14615f24805912c092b23a73509965aa8247b25
Toby Simpson <toby.simpson@usi.ch>
2024-02-06 11:00:03 +0100

    Initial Commit

---
+++ test3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test3
+//
+//  Created by Toby Simpson on 06.02.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
