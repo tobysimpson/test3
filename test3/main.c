5bb75a5792e0e8fb7f558d4e75f98fe8cd5ceb9e
tobysimpson <tobygeneva@hotmail.com>
2024-05-13 11:56:25 +0200

    create from coord and solve works ok?

--- test3/main.c
+++ test3/main.c
@@ -10,44 +10,57 @@
 #include <OpenCL/opencl.h>
 
 
-//trying block sparse, unions on host - copied from test13_solve3.c in test1
+//re-learning sparse solvers
 
-#define ROOT_WRITE  "/Users/toby/Downloads/"
-
-union flt4
+void fn_disp(float v[4])
 {
-    cl_float4  vec;
-    float      arr[4];
+    for(int i=0; i<4; i++)
+    {
+        printf("%f\n",v[i]);
+    }
+}
 
-};
-
-
-union flt4x4
+void fn_print_csr(SparseMatrix_Float A)
 {
-    cl_float16  vec;
-    float       arr[4][4];
-};
-
-
-//write
-void wrt_raw(void *ptr, size_t n, size_t bytes, char *file_name)
-{
-//    printf("%s\n",file_name);
+    float aa[A.structure.rowCount*A.structure.columnCount];
     
-    //name
-    char file_path[250];
-    sprintf(file_path, "%s%s.raw", ROOT_WRITE, file_name);
-
-    //open
-    FILE* file = fopen(file_path,"wb");
-  
-    //write
-    fwrite(ptr, bytes, n, file);
+    //reset
+    for(int i=0; i<A.structure.rowCount*A.structure.columnCount; i++)
+    {
+        aa[i] = 0;
+    }
     
-    //close
-    fclose(file);
     
-    return;
+    for(int col_idx=0; col_idx<A.structure.columnCount; col_idx++)
+    {
+//        printf("%ld %ld\n", A.structure.columnStarts[col_idx], A.structure.columnStarts[col_idx+1]);
+        
+        for(long row_ptr=A.structure.columnStarts[col_idx]; row_ptr<A.structure.columnStarts[col_idx+1]; row_ptr++)
+        {
+//            printf("%lu ", row_ptr);
+            
+            int row_idx = A.structure.rowIndices[row_ptr];
+            float   val = A.data[row_ptr];
+            
+            aa[row_idx*A.structure.columnCount+col_idx] = val;
+        }
+        
+//        printf("\n");
+    }
+    
+    
+    
+    for(int j=0; j<A.structure.columnCount; j++)
+    {
+        for(int i=0; i<A.structure.rowCount; i++)
+        {
+            printf("%f ",aa[j*A.structure.columnCount+i]);
+        }
+        printf("\n");
+    }
+    
+    
+    
 }
 
 
@@ -56,65 +69,60 @@ int main(int argc, const char * argv[])
 {
     printf("hello\n");
     
+
     //vec
-    union flt4 uu[4];
-    union flt4 bb[4];
+    float uu[4] = {1,1,1,1};
+    float bb[4] = {0,0,0,0};
     
-    DenseVector_Float u = {16, (float*)uu};
-    DenseVector_Float b = {16, (float*)bb};
+    
+    DenseVector_Float u = {4, uu};
+    DenseVector_Float b = {4, bb};
 
     //coo
-    int             ii[4] = {0,1,2,3};
-    int             jj[4] = {0,1,2,3};
-    union flt4x4  vv[4];
+    int     ii[6] = {0,1,2,3,0,1};
+    int     jj[6] = {0,1,2,3,1,0};
+    float   vv[6] = {1,1,1,1,2,3};
 
     
     //init
-    for(int k=0; k<4; k++)
-    {
-        for(int j=0; j<4; j++)
-        {
-            uu[k].arr[j] = 4*k+j;
-            bb[k].arr[j] = 0e0f;
-            
-            for(int i=0; i<4; i++)
-            {
-                vv[k].arr[j][i] = 4*i+j;    //col maj
-                vv[k].arr[j][i] = 2*(i==j);    //col maj
-            }
-        }
-    }
-    
-    //write vec
-    wrt_raw(uu, 4, sizeof(cl_float4),  "uu");
-    wrt_raw(bb, 4, sizeof(cl_float4),  "bb");
-    
-    //write mtx
-    wrt_raw(ii, 4, sizeof(int),         "A_ii");
-    wrt_raw(jj, 4, sizeof(int),         "A_jj");
-    wrt_raw(vv, 4, sizeof(cl_float16),  "A_vv");
-    
     SparseAttributes_t atts;
-    atts.kind = SparseOrdinary;
+//    atts.kind = SparseOrdinary;
+//    atts.kind = SparseSymmetric;            //sums and stores by triangle type
+    atts.kind = SparseTriangular;         //discards according to triangle type, default is upper
     
-    long    blk_num = 4;
-    UInt8   blk_sz  = 4;
+//    atts.triangle = SparseLowerTriangle;
+//    atts.triangle = SparseUpperTriangle;
+    
+
+    
+    long    nnz     = 6;
+    UInt8   blk_sz  = 1;
     
     int n = 4;
     
-    SparseMatrix_Float A = SparseConvertFromCoordinate(n, n, blk_num, blk_sz, atts, ii, jj, (float*)vv);  //duplicates sum
+    SparseMatrix_Float A = SparseConvertFromCoordinate(n, n, nnz, blk_sz, atts, ii, jj, vv);  //duplicates sum
     
+    //over-ride after init!!!
+    A.structure.attributes.kind = SparseSymmetric;
     
+    fn_print_csr(A);
+    
+    //mult
     SparseMultiply(A,u,b);
+    fn_disp(b.data);
     
-    memset(uu,0, 4*sizeof(cl_float4));
+    //reset
+    memset(u.data,0, 4*sizeof(float));
+//    fn_disp(u.data);
+    
+    
     
     SparseSolve(SparseGMRES(), A, b, u);
+//    SparseSolve(SparseConjugateGradient(), A, b, u);
     SparseCleanup(A);
 
-    //write vec
-    wrt_raw(uu, 4, sizeof(cl_float4),  "uu");
-    wrt_raw(bb, 4, sizeof(cl_float4),  "bb");
+
+    fn_disp(u.data);
     
     
     printf("done\n");
